80889a621aaab3cae4efda7476792048c18a991d
imranilyas <68575807+imranilyas@users.noreply.github.com>
2021-10-18 07:31:05 -0700

    Routing thro Application

---
+++ src/Screens/RootScreen.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+// Icons
+import { MdHome, /*MdWork,*/ MdEmail } from 'react-icons/md';
+import { IoMdContact } from 'react-icons/io';
+import { CgNotes } from 'react-icons/cg';
+
+// Components and styling
+import Contact from "../Components/Contact/Contact";
+import Home from "../Components/Home/Home";
+// import Experience from "../Components/Experience/Experience";
+import Projects from "../Components/Projects/Projects";
+import Background from "../Components/Background/Background";
+import './RootScreen.css';
+
+const RootScreen = () => {
+    return(
+        <>
+            <Router className = 'wholescreen'>
+                {/* Navbar on the left side */}
+                <div className = 'left'>
+                    <Link to='/' className = 'link'>
+                        <div className = 'whole-icon'>
+                            <MdHome className = 'icons'/> 
+                            <p className = 'description'>Home</p>
+                        </div>
+                    </Link>
+
+                    <Link to='/background' className = 'link'>
+                        <div className = 'whole-icon'>
+                            <IoMdContact className = 'icons'/>
+                            <p className = 'description'>Background</p>
+                        </div>
+                    </Link>
+
+                    {/* Will Add this section when I have worked at more companies */}
+                    {/* Will fill it with pressable logos of each company */}
+                    {/* <Link to='/experience' className = 'link'>
+                        <div className = 'whole-icon'>
+                            <MdWork className = 'icons'/>
+                            <p className = 'description'>Experience</p>
+                        </div>
+                    </Link> */}
+
+                    <Link to='/projects' className = 'link'>
+                        <div className = 'whole-icon'>
+                            <CgNotes className = 'icons' />
+                            <p className = 'description'>Projects</p>
+                        </div>
+                    </Link>
+
+                    <Link to='/contact' className = 'link'>
+                        <div className = 'whole-icon'>
+                            <MdEmail className = 'icons'/>
+                            <p className = 'description'>Contact</p>
+                        </div>
+                    </Link>
+                </div>
+                
+                {/* Content on the right side of the screen */}
+                <div className = 'right'>
+                    <Switch>
+                        <Route exact path = '/'>
+                            <Home />
+                        </Route>    
+                        <Route path = '/background'>
+                            <Background />
+                        </Route>
+                        <Route path = '/projects'>
+                            <Projects />
+                        </Route>
+                        <Route path = '/contact'>
+                            <Contact />
+                        </Route>
+                        {/* <Route path = '/experience'>
+                            <Experience />
+                        </Route> */}
+                    </Switch>
+                </div>
+            </Router>
+        </>
+    )
+}
+
+export default RootScreen;
